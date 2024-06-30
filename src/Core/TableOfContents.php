ffd213f00745e014a944044397ec22cf3efe4551
Wasseem Khayrattee <hey@wk.contact>
2024-06-30 17:01:37 +0400

    feat: Added customizable heading levels for TOC using top_level and depth shortcode attributes + Added SiteNavigationElement schema to enhance SEO with an option to enable/disable it via shortcode attribute schema

--- src/Core/TableOfContents.php
+++ src/Core/TableOfContents.php
@@ -28,19 +28,29 @@ class TableOfContents
             return '';
         }
 
+        // Set default values for top_level and depth
+        $top_level = isset($attributes['top_level']) ? (int) $attributes['top_level'] : 1;
+        $depth = isset($attributes['depth']) ? (int) $attributes['depth'] : 6;
+        $schema_enabled = isset($attributes['schema']) ? filter_var($attributes['schema'], FILTER_VALIDATE_BOOLEAN) : true;
+
         // This ensures that all header tags have `id` attributes so they can be used as anchor links
         $fixer = new MarkupFixer();
         $content = $fixer->fix($content);
 
         /** @var TocGenerator $toc_content */
         $generator = new TocGenerator();
-        $toc_html = $generator->getHtmlMenu($content);
+        $toc_html = $generator->getHtmlMenu($content, $top_level, $depth);
+
+        $json_ld = '';
+        if ($schema_enabled) {
+            $json_ld = self::generate_json_ld($content, $top_level, $depth);
+        }
 
         ob_start();
         $tpl_args = ['toc' => $toc_html];
         load_template(CLARAPRESS_TOC_PLUGIN_DIR . 'templates/toc.tpl.php', false, $tpl_args);
 
-        return ob_get_clean();
+        return ob_get_clean() . $json_ld;
     }
 
     public static function fix_content_by_adding_anchors(): string
@@ -56,5 +66,30 @@ class TableOfContents
         }
 
         return $post_content;
+    }
+
+    private static function generate_json_ld(string $content, int $top_level, int $depth): string
+    {
+        $generator = new TocGenerator();
+        $menu = $generator->getMenu($content, $top_level, $depth);
+
+        $items = [];
+        foreach ($menu->getChildren() as $child) {
+            $items[] = [
+                '@type' => 'SiteNavigationElement',
+                'name' => $child->getLabel(),
+                'url' => get_permalink() . '#' . $child->getName(),
+            ];
+        }
+
+        $data = [
+            '@context' => 'https://schema.org',
+            '@type' => 'SiteNavigationElement',
+            'name' => 'Table of Contents',
+            'url' => get_permalink(),
+            'hasPart' => $items,
+        ];
+
+        return '<script type="application/ld+json">' . json_encode($data, JSON_UNESCAPED_SLASHES) . '</script>';
     }
 }
