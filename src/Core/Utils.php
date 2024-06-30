2b0e4a10ce940c81d523b03e9ce9166bfc91ff51
Wasseem Khayrattee <hey@wk.contact>
2024-07-01 01:28:56 +0400

    fix: date() is affected by runtime timezone changes which can cause date/time to be incorrectly displayed. Use gmdate() instead.

--- src/Core/Utils.php
+++ src/Core/Utils.php
@@ -11,7 +11,7 @@ class Utils
      */
     public static function log(string $level, string $message, array $context = []): void
     {
-        error_log(PHP_EOL . date('F j, Y, g:i a e O') . ' | ' . mb_strtoupper($level) . ': ' . $message . ' ' .
+        error_log(PHP_EOL . gmdate('F j, Y, g:i a e O') . ' | ' . mb_strtoupper($level) . ': ' . $message . ' ' .
             (!empty($context) ? print_r($context, true) : ''), 3, CLARAPRESS_TOC_PLUGIN_ERROR_LOG_FILE);
     }
 }
