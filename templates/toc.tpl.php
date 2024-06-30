ba8def7c5edac1e760b2c4359b0dc1d2e520bb9f
Wasseem Khayrattee <hey@wk.contact>
2024-06-30 16:59:02 +0400

    feat: Implement a filter clarapress_toc_summary_text to allow overriding the summary text in the TOC + Added localization support for summary text using the __() function and text domain clarapress-toc

--- templates/toc.tpl.php
+++ templates/toc.tpl.php
@@ -4,12 +4,15 @@ $toc_content = '';
 if (isset($injected_args['toc'])) {
     $toc_content = $injected_args['toc'];
 }
+
+// Apply the filter to allow overriding the summary text
+$summary_text = apply_filters('clarapress_toc_summary_text', __('Open Table of contents', 'clarapress-toc'));
 ?>
 <div class="clarapress-toc-wrapper">
     <details>
-        <summary>Open Table of contents</summary>
+        <summary><?php echo esc_html($summary_text); ?></summary>
         <div class='clarapress-toc-items'>
-            <?php echo $toc_content;?>
+            <?php echo $toc_content; ?>
         </div>
     </details>
 </div>
